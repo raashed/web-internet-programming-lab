9570cfe0a930bfb2526af09e05e18cd1af1cbc9b
raashed <rashed.rsd4@gmail.com>
2022-10-22 12:05:49 +0600

    all done code

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 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
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var LocalStorage = require('node-localstorage').LocalStorage,
+    localStorage = new LocalStorage('./scratch');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    var login = localStorage.getItem('login')
+    res.render('login', {title: 'Express', login: login});
 });
 
+router.post('/login', function (req, res, next) {
+    if (req.body.email == undefined || req.body.email.length == 0) {
+        return res.render('login', {title: 'Express', email: false});
+    }
+    if (req.body.password == undefined || req.body.password.length == 0) {
+        return res.render('login', {title: 'Express', password: false});
+    }
+
+    localStorage.setItem('login', 'true')
+    return res.redirect('/')
+});
+
+router.get('/logout', function (req, res, next) {
+    localStorage.removeItem('login')
+
+    return res.redirect('/')
+})
 module.exports = router;
